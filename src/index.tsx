16ff7d33d52e741eeb9a294affb66f3ac470902b
Eric Youngmin Song <dudals2900@gmail.com>
2024-04-07 23:10:16 -0400

    Function Comp

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 // 1) Import ReactDOM library
-import ReactDOM from "react-dom/client";
- 
+import ReactDOM from 'react-dom/client';
+import GuestList from './state/GuestList';
+import UserSearch from './state/UserSearch';
+import EventComponent from './events/EventComponent';
+
 // 2) Get a reference to the div with ID root
-const el = document.getElementById("root");
- 
+const el = document.getElementById('root');
+
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
-  return <div>
-    <h1>Hi there!</h1>
-  </div>
+  return (
+    <div>
+      <GuestList />
+      <UserSearch />
+      <EventComponent />
+    </div>
+  );
 };
 
 root.render(<App />);
---
+++ src/props/Child.tsx
@@ -0,0 +1,29 @@
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+  // React 18 Change
+  children?: React.ReactNode;
+}
+
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+};
+
+export const ChildAsFC: React.FunctionComponent<ChildProps> = ({
+  color,
+  onClick,
+  children,
+}) => {
+  return (
+    <div>
+      {color}
+      {children}
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+};
