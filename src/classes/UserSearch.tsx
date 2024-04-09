2511264f9609f87b8113643b3566db4249284955
Eric Youngmin Song <dudals2900@gmail.com>
2024-04-08 23:27:27 -0400

    Class Component

---
+++ src/classes/UserSearch.tsx
@@ -0,0 +1,49 @@
+import { Component } from 'react';
+
+interface User {
+  name: string;
+  age: number;
+}
+
+interface UserSearchProps {
+  users: User[];
+}
+
+interface UserSearchState {
+  name: string;
+  user: User | undefined;
+}
+
+class UserSearch extends Component<UserSearchProps> {
+  state: UserSearchState = {
+    name: '',
+    user: undefined,
+  };
+
+  onClick = () => {
+    const foundUser = this.props.users.find((user) => {
+      return user.name === this.state.name;
+    });
+    this.setState({ user: foundUser });
+  };
+
+  redner() {
+    const { name, user } = this.state;
+    return (
+      <div>
+        <h3>User Search</h3>
+        <input
+          value={name}
+          onChange={(e) => this.setState({ name: e.target.value })}
+        />
+        <button onClick={this.onClick}>Find User</button>
+        <div>
+          {user && user.name}
+          {user && user.age}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserSearch;
